31a122676b43ee23510c6740b584a1c743d53a68
DESKTOP-BB9KND1\ghoofman <ghoofman@ostusa.com>
2017-03-01 12:23:46 -0500

    Fixing models

--- app/js/editor/Projectbar.Models.js
+++ app/js/editor/Projectbar.Models.js
@@ -12,23 +12,31 @@
 	var root = {
 		models: []
 	};
-	var rootFolders = {};
+    var rootFolders = {};
+    var lastPath = '';
 
 	var modelsContainer = new UI.Panel();
 
-  function GatherModels() {
-		models = [];
-		modelsContainer.dom.innerHTML = '';
+    function GatherModels(pathToLoad) {
+        rootFolders = {};
+        models = [];
+        var root = {
+            models: []
+        };
+        modelsContainer.dom.innerHTML = '';
 
 		var fs = require('fs');
 		if(!fs.existsSync(window.projectPath + '/Assets/Models/')) {
 			fs.mkdirSync(window.projectPath + '/Assets/Models/');
 		}
 
-		rootPath = window.projectPath + '/Assets/Models/';
+        rootPath = window.projectPath + '/Assets/Models/';
+        if (!lastPath) {
+            lastPath = null;
+        }
 
 		rootPath = rootPath.split('\\').join('/');
-	  OPIFEX.Utils.Walk(rootPath, function(result, name, stat, notInRoot, path) {
+	    OPIFEX.Utils.Walk(rootPath, function(result, name, stat, notInRoot, path) {
 			if(!stat.isDirectory() && !name.endsWith('.opm')) return;
 
 			if(notInRoot) {
@@ -38,7 +46,8 @@
 
 					rootFolders[path] = {
 						models: [],
-						parent: parent.join('/')
+                        parent: parent.join('/'),
+                        path: path
 					};
 				}
 
@@ -59,7 +68,7 @@
 					parent: path != rootPath ? path : null
 				});
 			}
-	  });
+	    });
 
     modelsContainer.setClass('ModelViewerPanel');
 
@@ -70,9 +79,9 @@
 
 			if(folder) {
 				var backButton = document.createElement( 'a' );
-		    backButton.className = 'Model Back';
+		        backButton.className = 'Model Back';
 				backButton.textContent = 'Back';
-		    modelsContainer.dom.appendChild(backButton);
+		        modelsContainer.dom.appendChild(backButton);
 				backButton.addEventListener( 'click', function(event) {
 					if(!folder) {
 						LoadModels();
@@ -84,7 +93,9 @@
 
 			if(!folder) {
 				folder = root;
-			}
+            }
+
+            lastPath = folder;
 
 			folder.models.sort(function(a,b) {
 				if(a.folder && !b.folder) return -1;
@@ -94,44 +105,48 @@
 				return 0;
 			});
 
-		  for(var i = 0; i < folder.models.length; i++) {
+		    for(var i = 0; i < folder.models.length; i++) {
 				var model = document.createElement( 'a' );
-		    model.className = 'Model';
-		    model.setAttribute('model-name', folder.models[i].asset);
-		    model.setAttribute('model-type', 'model');
-		    model.setAttribute('model-path', folder.models[i].path);
+		        model.className = 'Model';
+		        model.setAttribute('model-name', folder.models[i].asset);
+		        model.setAttribute('model-type', 'model');
+		        model.setAttribute('model-path', folder.models[i].path);
 				model.setAttribute('style', "background-size: cover; background-image: url('" + folder.models[i].path.split('\\').join('/') + ".png')");
 				model.textContent = folder.models[i].name;
 				if(folder.models[i].folder) {
 					model.className = 'Model Folder';
-			  	model.addEventListener( 'click', function(event) {
+			  	    model.addEventListener( 'click', function(event) {
 						console.log('Clicked Folder');
-				    var path = this.getAttribute('model-path');
+				        var path = this.getAttribute('model-path');
 						LoadModels(rootFolders[path]);
 					});
 				} else {
 		    	model.setAttribute('draggable', 'true');
 			  	model.addEventListener( 'dragstart', function(event) {
-			      var name = this.getAttribute('model-name');
-			      var t = this.getAttribute('model-type');
-			      event.dataTransfer.setData("text", name);
-			      event.dataTransfer.setData("type", t);
+			        var name = this.getAttribute('model-name');
+			        var t = this.getAttribute('model-type');
+			        event.dataTransfer.setData("text", name);
+			        event.dataTransfer.setData("type", t);
 			    }, false );
 				}
-		    modelsContainer.dom.appendChild(model);
+		        modelsContainer.dom.appendChild(model);
 			}
 		}
 
-		LoadModels();
-
+        if (pathToLoad) {
+            LoadModels(rootFolders[pathToLoad.path]);
+        } else {
+            LoadModels(null);
+        }
+            
 	}
 
 	require('fs').watch(window.projectPath + '/Assets/Models/', function(event, filename) {
 		console.log('File change', event, filename);
-		GatherModels();
+        GatherModels(lastPath);
 	})
-
-	GatherModels();
+        
+	GatherModels(null);
 
   container.add(modelsContainer);
 
